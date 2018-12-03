d3123edecc967f9c23a912c70ae143609d7c9a12
xd009642 <danielmckenna93@gmail.com>
2018-12-03 22:08:11 +0000

    Added ReadType

--- src/process_handling/unix.rs
+++ src/process_handling/unix.rs
@@ -1,6 +1,5 @@
 use nix::sys::signal::Signal;
 use nix::sys::ptrace::*;
-use nix::libc::{c_void, c_long};
 use nix::unistd::Pid;
 use nix::{Error, Result};
 use nix::errno::Errno;
@@ -11,7 +10,12 @@ const RIP: u8 = 128;
 #[cfg(target_os = "macos")]
 type DataType = nix::libc::c_int;
 #[cfg(target_os = "linux")]
-type DataType = *mut c_void;
+type DataType = *mut nix::libc::c_void;
+
+#[cfg(target_os = "macos")]
+type ReadType = nix::libc::c_int;
+#[cfg(target_os = "linux")]
+type ReadType = nix::libc::c_long;
 
 #[cfg(target_os = "macos")]
 const POKE_USER:Request = Request::PT_WRITE_U;
@@ -35,7 +39,7 @@ pub fn single_step(pid: Pid) -> Result<()> {
     step(pid, None)
 }
 
-pub fn read_address(pid: Pid, address:u64) -> Result<c_long> {
+pub fn read_address(pid: Pid, address:u64) -> Result<ReadType> {
     read(pid, address as AddressType)
 }
 
@@ -45,19 +49,17 @@ pub fn write_to_address(pid: Pid,
     write(pid, address as AddressType, data as DataType)
 }
 
-#[allow(deprecated)]
-pub fn current_instruction_pointer(pid: Pid) -> Result<c_long> {
+pub fn current_instruction_pointer(pid: Pid) -> Result<ReadType> {
     let ret = unsafe {
         Errno::clear();
         libc::ptrace(PEEK_USER as RequestType, libc::pid_t::from(pid), RIP as AddressType, 0 as DataType)
     };
     match Errno::result(ret) {
         Ok(..) | Err(Error::Sys(Errno::UnknownErrno)) => Ok(ret.into()),
-        err @ Err(..) => err.map(|x| x as c_long),
+        err @ Err(..) => err.map(|x| x as ReadType),
     }
 }
 
-#[allow(deprecated)]
 pub fn set_instruction_pointer(pid: Pid, pc: u64) -> Result<()> {
     unsafe {
         Errno::clear();
